4948e0ec04009c05e4173355ffdd0181946a395f
dio aditya saputa <diodas34sby@gmail.com>
2024-10-01 16:22:35 +0700

    membuat website gudang antik abagh

---
+++ css/cobacoba.js
@@ -0,0 +1,7 @@
+// 1. prompt untuk mengetahui saldo akhir dari apa yang di input user
+
+let SaldoAwal = 3000;
+let SaldoTambahan = 13000;
+const SaldoAkhir = SaldoAwal + SaldoTambahan;
+
+alert(`saldo akhir saya sekarang adalah ${SaldoAkhir}`);
